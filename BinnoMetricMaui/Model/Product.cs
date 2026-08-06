14a534409516a80c332446a13e3240c9578e324f
Sergey Korolev <sergiussmithe@gmail.com>
2026-08-06 11:33:42 +0300

    Добавил модель продукта

---
+++ BinnoMetricMaui/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace BinnoMetricMaui.Model
+{
+    public class Product
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
