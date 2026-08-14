a038057f48c90dd1f38093c8ff16c88a90fbe633
Sergey Korolev <sergiussmithe@gmail.com>
2026-08-14 11:29:33 +0300

    Реализовал модель представления производственной записи

--- BinnoMetricMaui/ViewModel/ProductionRecordViewModel.cs
+++ BinnoMetricMaui/ViewModel/ProductionRecordViewModel.cs
@@ -1,7 +1,64 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BinnoMetricMaui.Model;
+using BinnoMetricMaui.Service;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 namespace BinnoMetricMaui.ViewModel;
 public partial class ProductionRecordViewModel : ObservableObject
 {
+    private readonly ProductionRecordService _productionRecordService;
+    public ProductionRecordViewModel(ProductionRecordService productionRecordService)
+    {
+        _productionRecordService = productionRecordService;
+        // Загружаем производственные записи при инициализации
+        _ = LoadProductinRecordsAsync();
+    }
+
     [ObservableProperty]
-    private string message = "mvvm записи производства";
+    private string logMessage = "";
+
+    [ObservableProperty]
+    private ObservableCollection<ProductionRecord> productionRecords = new ObservableCollection<ProductionRecord>();
+
+    [RelayCommand]
+    public async Task UpdatepPoductionRecordsList()
+    {
+        await LoadProductinRecordsAsync();
+    }
+
+    [RelayCommand]
+    public async Task DeleteProductinRecord(int id)
+    {
+        var result = await _productionRecordService.DeleteProductionRecordAsync(id);
+        if (result) LogMessage += $"\nПроизводственная запись с ID: {id} удалена";
+        else LogMessage += "\nНе удалось удалить производственную запись.";
+        await LoadProductinRecordsAsync();
+    }
+
+    private async Task LoadProductinRecordsAsync()
+    {
+        try
+        {
+            var productionRecordsList = await _productionRecordService.GetProductionRecordAsync();
+
+            ProductionRecords.Clear();
+            foreach (var record in productionRecordsList)
+            {
+                ProductionRecords.Add(record);
+            }
+
+            LogMessage += $"\nЗагружено {ProductionRecords.Count} продуктов";
+        }
+        catch (Exception ex)
+        {
+            LogMessage += $"\nОшибка загрузки: {ex.Message}";
+        }
+    }
+
+    [RelayCommand]
+    private void CLearLogMessage()
+    {
+        LogMessage = "";
+    }
+
 }
