866d1cd3124037071592425ae372a5df9211357d
Sergey Korolev <sergiussmithe@gmail.com>
2026-08-18 14:57:10 +0300

    Реализовал красивое отображение карточки производственной записи  и всех данных

--- BinnoMetricMaui/ViewModel/ProductionRecordCardViewModel.cs
+++ BinnoMetricMaui/ViewModel/ProductionRecordCardViewModel.cs
@@ -1,12 +1,22 @@
 ﻿using BinnoMetricMaui.Model;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace BinnoMetricMaui.ViewModel;
 
 public partial class ProductionRecordCardViewModel : ObservableObject
 {
-    private readonly ProductionRecord _card;
+    [ObservableProperty]
+    private ProductionRecord card;
+
     public ProductionRecordCardViewModel(ProductionRecord card)
     {
-        _card = card;
+        Card = card;
+    }
+
+    [RelayCommand]
+    private async Task GoBack()
+    {
+        await Shell.Current.GoToAsync("..");
     }
 }
